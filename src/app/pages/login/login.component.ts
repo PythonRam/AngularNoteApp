b026d8ea698f665b11be15f454ce028e9dc0c227
pythonRam <python.hithesh@gmail.com>
2018-09-27 01:55:53 +0530

    everything done

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { LoginForm } from '../../models/LoginForm';
+import { UserService } from '../../services/user.service';
+import { LoginResponse } from '../../models/LoginResponse';
+import { MessageService } from 'primeng/api';
+import { Subscription } from 'rxjs';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: LoginForm;
+  lgnSubscription: Subscription;
+  @ViewChild('lgnButton') lgnButton: ElementRef;
+  constructor(private userService: UserService, private messageService: MessageService, private router: Router) {
+    this.loginForm = new LoginForm();
+    this.userService.logout();
+    this.userService.userLoggedInSubject.subscribe((userLoggedIn: boolean) => {
+      if (userLoggedIn) {
+        this.messageService.add({ severity: 'success', summary: 'Successfully logged in' });
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.messageService.add({ severity: 'info', summary: 'Wrong Credentials' });
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  login(event: MouseEvent) {
+    this.lgnButton.nativeElement.disabled = true;
+    try {
+      this.userService.login(this.loginForm);
+      this.lgnButton.nativeElement.disabled = false;
+    } catch (error) {
+      this.lgnButton.nativeElement.disabled = false;
+      this.lgnSubscription.unsubscribe();
+      this.messageService.add({ severity: 'error', summary: error.message });
+    }
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,26 +9,36 @@ import { LoginResponse } from '../models/LoginResponse';
 export class UserService {
   private baseUrl = 'http://localhost:3000';
   public userLoggedIn: boolean;
+  public userLoggedInSubject: ReplaySubject<boolean> = new ReplaySubject();
 
   private options = { headers: new HttpHeaders().set('Content-Type', 'application/json') };
   constructor(private httpClient: HttpClient) {
     if (localStorage.getItem('uid')) {
       this.userLoggedIn = true;
+      this.userLoggedInSubject.next(this.userLoggedIn);
+
     } else {
       this.userLoggedIn = false;
+      this.userLoggedInSubject.next(this.userLoggedIn);
     }
   }
 
-  login(loginForm: LoginForm) {
+  login(loginForm: LoginForm): ReplaySubject<boolean> {
     this.httpClient.post(this.baseUrl + '/login', loginForm, this.options).subscribe((loginResponse: LoginResponse) => {
       if (loginResponse.success) {
         localStorage.setItem('uid', loginResponse.uid);
         this.userLoggedIn = (true);
+        this.userLoggedInSubject.next(this.userLoggedIn);
+      } else {
+        this.userLoggedIn = false;
+        this.userLoggedInSubject.next(this.userLoggedIn);
       }
     });
+    return this.userLoggedInSubject;
   }
   logout() {
     localStorage.removeItem('uid');
     this.userLoggedIn = (false);
+    this.userLoggedInSubject.next(this.userLoggedIn);
   }
 }
